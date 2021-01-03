5d51278d547c58d3d913a1d4e836527d80c05a5b
Garrett Bjerkhoel <me@garrettbjerkhoel.com>
2021-01-02 21:56:26 -0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dewski/jsonpath
+
+go 1.14
---
+++ jsonpath_test.go
@@ -0,0 +1,69 @@
+package jsonpath
+
+import (
+	"sort"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSimplePathExamples(t *testing.T) {
+	jp := NewReader(map[string]interface{}{
+		"action": "user.login",
+		"issue": map[string]interface{}{
+			"title": "My issue",
+		},
+		"users": []map[string]interface{}{
+			{
+				"username": "dewski",
+			},
+		},
+	})
+
+	assert.Equal(t, "user.login", jp.Path(".action"))
+	assert.Equal(t, "My issue", jp.Path(".issue.title"))
+	assert.Equal(t, "dewski", jp.Path(".users[0].username"))
+}
+
+func TestSimplePaths(t *testing.T) {
+	jp := NewReader(map[string]interface{}{
+		"action": "user.login",
+		"issue": map[string]interface{}{
+			"title": "My issue",
+		},
+		"users": []map[string]interface{}{
+			{
+				"username": "dewski",
+			},
+		},
+	})
+
+	expected := []string{
+		".action",
+		".issue.title",
+		".users[0].username",
+	}
+	actual := jp.Paths()
+	sort.Strings(expected)
+	sort.Strings(actual)
+
+	assert.Equal(t, expected, actual)
+}
+
+func TestPathWithSpaces(t *testing.T) {
+	jp := NewReader(map[string]interface{}{
+		"event type": "user.login",
+		"some issues": map[string]interface{}{
+			"issue title": "My issue",
+		},
+		"all my users": []map[string]interface{}{
+			{
+				"user name": "dewski",
+			},
+		},
+	})
+
+	assert.Equal(t, "user.login", jp.Path(`."event type"`))
+	assert.Equal(t, "My issue", jp.Path(`."some issues"."issue title"`))
+	assert.Equal(t, "dewski", jp.Path(`."all my users"[0]."user name"`))
+}
